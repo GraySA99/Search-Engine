850961fc1750251897bc538464f7fcc667ec8265
Spencer Gray <grayspa17@gmail.com>
2021-04-06 09:13:21 -0400

    Page Ranking Done. Can't test yet

--- SearchEngine/src/main/scala/PageRank.scala
+++ SearchEngine/src/main/scala/PageRank.scala
@@ -4,16 +4,24 @@ import scala.collection.parallel.CollectionConverters._
 object PageRank {
     def equal(pages: Map[String, WebPage]): Map[String, Double] = {
         // TODO: complete implementation
-        Map[String, Double]("hello" -> 1.0)
+        (pages.keys map { _ -> 1.0 }).toMap
     }
 
     def indegree(pages: Map[String, WebPage]): Map[String, Double] = {
         // TODO: complete implementation
-        Map[String, Double]("hello" -> 1.0)
+        pages.keys map (p -> (for (pages <- pages.filter{ _._1 != p }.valuesIterator if pages.links.contains(p)) yield 1).foldLeft(0){_+_})
     }
 
     def pagerank(pages: Map[String, WebPage]): Map[String, Double] = {
         // TODO: complete implementation
-        Map[String, Double]("hello" -> 1.0)
+
+        def walk(page: WebPage, steps: Int): List[String] = steps match {      
+
+            case x if x <= 0 => List[String]()
+            case _ => List[String](page.id) ++ walk((page.links.length == 0 || Random.nextFloat <= 0.15) ? pages.valuesIterator[Random.nextInt(pages.valuesIterator.length)] : page.links[Random.nextInt(pages.links.length)], steps-1)
+        } 
+
+        val x: List[String] = (for (_ <- 0 until 10000) yield walk(pages.valuesIterator[Random.nextInt(pages.valuesIterator.length)])).foldLeft(List[String]()){_++_}
+        (x map {t => t -> x.count { _ == t } }).toMap
     }
 }
