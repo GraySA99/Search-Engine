ea7c07759c456624d6be46cb1dff1928b56aef04
Spencer Gray <grayspa17@gmail.com>
2021-04-14 15:39:01 -0400

    Page Rank Error Fixes

--- SearchEngine/src/main/scala/PageRank.scala
+++ SearchEngine/src/main/scala/PageRank.scala
@@ -9,7 +9,7 @@ object PageRank {
 
     def indegree(pages: Map[String, WebPage]): Map[String, Double] = {
         // TODO: complete implementation
-        pages.keys map (p -> (for (pages <- pages.filter{ _._1 != p }.valuesIterator if pages.links.contains(p)) yield 1).foldLeft(0){_+_})
+        (pages.keys map { p => p -> 1.0*((for (pages <- pages.filter{ _._1 != p }.valuesIterator if pages.links.contains(p)) yield 1).foldLeft(0){_+_}) }).toMap
     }
 
     def pagerank(pages: Map[String, WebPage]): Map[String, Double] = {
@@ -18,10 +18,18 @@ object PageRank {
         def walk(page: WebPage, steps: Int): List[String] = steps match {      
 
             case x if x <= 0 => List[String]()
-            case _ => List[String](page.id) ++ walk((page.links.length == 0 || Random.nextFloat <= 0.15) ? pages.valuesIterator[Random.nextInt(pages.valuesIterator.length)] : page.links[Random.nextInt(pages.links.length)], steps-1)
+            case _ => {
+                if (page.links.length == 0 || Random.nextFloat <= 0.15) {
+                    val w = pages.values.toList(Random.nextInt(pages.values.size))
+                    List[String](page.id) ++ walk(w, steps-1)
+                } else {
+                    val w = page.links(Random.nextInt(page.links.length))
+                    List[String](page.id) ++ walk(pages(w), steps-1)
+                }
+            } 
         } 
 
-        val x: List[String] = (for (_ <- 0 until 10000) yield walk(pages.valuesIterator[Random.nextInt(pages.valuesIterator.length)])).foldLeft(List[String]()){_++_}
-        (x map {t => t -> x.count { _ == t } }).toMap
+        val x: List[String] = (for (_ <- 0 until 10000) yield walk(pages.values.toList(Random.nextInt(pages.values.size)), 100)).foldLeft(List[String]()){_++_}
+        (x map {t => t -> (x.count { _ == t })*1.0 }).toMap
     }
 }
