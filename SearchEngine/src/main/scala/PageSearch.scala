049503b9d181777ab842e6eb44f8738ce83d184f
PJKnightR <coolpj99@comcast.net>
2021-04-13 21:21:58 -0400

    Implemented 2/3 or search

--- SearchEngine/src/main/scala/PageSearch.scala
+++ SearchEngine/src/main/scala/PageSearch.scala
@@ -1,15 +1,26 @@
+import scala.annotation.tailrec
+import scala.collection.parallel.CollectionConverters.ImmutableIterableIsParallelizable
+import scala.collection.parallel.ParIterable
 import scala.math.log
-import scala.collection.parallel.CollectionConverters._
 
 object PageSearch {
+    def contains(query: ParIterable[String], string: String): Boolean = {
+        query.foldLeft(false)((a, b) => if (b == string) true else false)
+    }
+
+    @tailrec
+    def countTerms(termList: List[String], query: List[String], sum: Int): Int = termList match {
+        case Nil => sum
+        case h::t if(contains(query.par, h)) => countTerms(t, query, sum + 1)
+        case h::t => countTerms(t, query, sum)
+    }
+
     def count(pages: List[RankedWebPage], query: List[String]): List[Double] = {
-        // TODO: complete implementation
-        List[Double](1.0)
+        (for (x <- pages) yield countTerms(x.text.split("\\s+").toList, query, 0))
     }
 
     def tf(pages: List[RankedWebPage], query: List[String]): List[Double] = {
-        // TODO: complete implementation
-        List[Double](1.0)
+        (for (x <- pages) yield countTerms(x.text.split("\\s+").toList, query, 0) / x.text.split("\\s+").toList.size)
     }
 
     def tfidf(pages: List[RankedWebPage], query: List[String]): List[Double] = {
