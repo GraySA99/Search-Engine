349a1f4c25e065292c0265a95f01bf07c7257d70
gabearmpa <55257018+gabearmpa@users.noreply.github.com>
2021-04-15 10:56:06 -0400

    update AskWillie

--- SearchEngine/src/main/scala/AskWillie.scala
+++ SearchEngine/src/main/scala/AskWillie.scala
@@ -1,5 +1,6 @@
 import scala.io.Source
 import scala.io.StdIn.readLine
+import scala.util.Sorting
 
 object AskWillie {
     def main(args: Array[String]) = {
@@ -14,16 +15,43 @@ object AskWillie {
 
         // Load WebPage.id -> WebPage map to better handle graph
         val pages: Map[String, WebPage] = mapWebPages(loadWebPages)
+        //print("not")
+        val ranks: Map[String, Double] = PageRank.pagerank(pages)
 
-        print("|-/ ")
-        var userInput = readLine()
+        //print("here")
+
+        val max = ranks.max._2
+        val min = ranks.min._2
+
+        val normalRank = for((key,value) <- ranks) yield (key -> (value-min)/(max-min))
+
+        val rankedPages = for((key, value) <- pages) yield new RankedWebPage(value.id, value.name, value.url, value.text, value.links, normalRank.getOrElse(key, 0))
+
+        
+        var userInput = ""
 
         while (userInput != ":quit") {
             print("|-/ ")
-            userInput = readLine()
+            userInput = readLine()  
+            val words = userInput.split(" ")
+            val matches = PageSearch.count(rankedPages.toList, words.toList)
+            val normalMatch = for(value <- matches) yield ((value-matches.min)/(matches.max-matches.min))
+            print(rankedPages)
+            print(words)
+            print("results:"+matches)
+
+            val searchPages = for((page, matches) <- rankedPages zip normalMatch) yield new SearchedWebPage(page.id, page.name, page.url, page.text, page.links, page.weight, matches)
+
+            //val pageTuple = Array(for(page <- searchPages) yield (page, Arithmetic(page.weight, page.textmatch)))
+            //check _._2 please
+            //val pageSort = Sorting.quickSort(pageTuple)(Ordering.by[SearchedWebPage, Double](_._2))
         }
 
     }
+
+    def Arithmetic(weight: Double, rank: Double) = {(weight + rank)/2}
+    def Geometric(weight: Double, rank: Double) = {math.sqrt(weight * rank)}
+    def harmonic(weight: Double, rank: Double) = {2/((1/weight) + (1/rank))}
 
     // Load a List of WebPage objects from the packaged prolandwiki.csv file
     def loadWebPages: List[WebPage] = {
